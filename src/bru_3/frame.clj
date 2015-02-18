7635c7434678edc125c5ff99a0a96e1aba2f5bc1
Vladimir Mitrovic <vladimirm@nordeus.eu>
2015-02-18 22:42:20 +0100

    Minor edits.

--- src/bru_3/frame.clj
+++ src/bru_3/frame.clj
@@ -6,18 +6,18 @@
   (vertices [this] (vec (map (partial get this) [:tl :tr :bl :br])))
   (edges [this]
     (let [edge (fn [[v1 v2]] (vec (map (partial get this) [v1 v2])))
-          pairs [[:tl :tr] [:tr :bl] [:bl :br] [:br :tl]]]
+          pairs [[:tl :tr] [:tr :br] [:br :bl] [:bl :tl]]]
       (map edge pairs))))
 
 (defn from-bones
-  "Creates a frame in between the two given bones b1 and b2, using the randomization maximum rm to offset the bone
-  vertices. rm is given as a percentage in the [0, 1] range. When rm = 0, there'll be no vertex randomization, and the
+  "Creates a frame in between the two given bones b1 and b2, using the randomization maximum r to offset the bone
+  vertices. r is given as a percentage in the [0, 1] range. When r = 0, there'll be no vertex randomization, and the
   frame will fit exactly between the two bones. For any other value, a random percentage will be added or subtracted
   from the bone lengths, and the frame will be constructed using the resulting shortened/elongated bones."
-  [b1 b2 rp]
+  [b1 b2 r]
   (let [;; make a function for getting randomized length in [-rp, rp] range
         rand-range (fn [min max] (+ min (rand (- max min))))
-        rfn (fn [] (rand-range (- rp) rp))
+        rfn (fn [] (rand-range (- r) r))
         ;; randomize each bone length
         rb1 (update-in b1 [:length] * (rfn))
         rb2 (update-in b2 [:length] * (rfn))]
